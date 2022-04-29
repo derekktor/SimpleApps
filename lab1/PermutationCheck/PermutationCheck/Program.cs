3da0f40da623eb6f6846ac698abb5f47e94b9c57
Derek <baldka1109bh@gmail.com>
2022-04-29 22:31:12 +0200

    Finish PermutationCheck App

--- lab1/PermutationCheck/PermutationCheck/Program.cs
+++ lab1/PermutationCheck/PermutationCheck/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PermutationCheck
 {
@@ -6,7 +7,70 @@ namespace PermutationCheck
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Domain = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "/inputs";
+            string FileCat = "/incorrect";
+            string FileName = "/no-perm-1-3.txt";
+
+            string[] input = GetLines(Domain, FileCat, FileName);
+
+            int[] numbers = GetNumbers(input);
+
+            bool hasPermutation = CheckPermutation(numbers);
+
+            Console.WriteLine(hasPermutation);
+         
+            
         }
+
+        public static bool CheckPermutation(int[] numbers)
+        {
+            int score = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1] - 1)
+                {
+                    score++;
+                }
+            }
+
+            if (score == numbers.Length - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int[] GetNumbers(string[] input)
+        {
+            bool validN = int.TryParse(input[0], out int n);
+
+            int[] numbers = new int[n];
+            string[] texts = input[1].Split(" ");
+
+            if (!validN || texts.Length != n)
+            {
+                throw new Exception("Invalid n!");
+            }
+
+            int index = 0;
+            foreach (string num in texts)
+            {
+                bool isValidNum = int.TryParse(num, out int numInList);
+                numbers[index++] = numInList;
+            }
+
+            return numbers;
+        }
+
+        public static string[] GetLines(string Domain, string FileCat, string FileName)
+        {
+            string FilePath = Domain + FileCat + FileName;
+            return File.ReadAllLines(FilePath);
+        }
+
+
     }
 }
