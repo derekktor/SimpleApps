677cf7721f42f78d0dc673c9b3e05dbc9a518dac
Derek <baldka1109bh@gmail.com>
2022-04-29 15:06:46 +0200

    Add LeapYear app
    The app takes in input: int from user and replies with either pleasant
    answer or rather quite an unpleasant one

---
+++ lab1-LeapYear/lab1-LeapYear/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace lab1_LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLeapYear;
+            bool wantAgain = true;
+
+            while (wantAgain)
+            {
+                Console.WriteLine("Enter a year:");
+
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput % 4 == 0)
+                {
+                    isLeapYear = true;
+                }
+                else
+                {
+                    isLeapYear = false;
+                }
+
+                if (isLeapYear)
+                {
+                    Console.WriteLine($"{userInput} is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine($"{userInput} is a NOT A FUCKING leap year");
+                }
+
+                Console.WriteLine("Want to try another year?");
+
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer.Equals("yes") || answer.Equals("yeah"))
+                {
+                    wantAgain = true;
+                } else
+                {
+                    wantAgain = false;
+                    Console.WriteLine("FUCK OFF CUNT!!!");
+                }
+
+            }
+
+            
+
+        }
+    }
+}
