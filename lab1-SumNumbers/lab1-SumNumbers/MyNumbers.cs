d2454217c2f47916f51cd5c305025f56a7def761
Derek <baldka1109bh@gmail.com>
2022-04-29 15:17:05 +0200

    Add Lab1 Sum Numbers App
    Simple Positive Numbers container
    App asks for numbers to be input
    As many numbers the user wants
    Entering negative numbers results in error
    MyNumbers class automatically increases buffer size

---
+++ lab1-SumNumbers/lab1-SumNumbers/MyNumbers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace lab1_SumNumbers
+{
+    public class MyNumbers
+    {
+        int[] _values;
+        int _buffer;
+        int _cursor;
+
+        public MyNumbers(int buffer = 5)
+        {
+            _values = new int[buffer];
+            _buffer = buffer;
+            _cursor = 0;
+        }
+
+        public void add(int num)
+        {
+            if (num < 0)
+            {
+                Console.WriteLine("Error: Can't enter negative number!!!");
+                return;
+            }
+
+            if (_cursor == _buffer)
+            {
+                _buffer = _cursor;
+                increaseSize();
+            }
+
+            _values[_cursor] = num;
+            _cursor++;
+        }
+
+        public string getInfo()
+        {
+            Console.WriteLine("HELLLOOOOO");
+
+            string result = "";
+
+            result += "Length: " + getLength() + "\n";
+            result += "Buffer: " + getBuffer() + "\n";
+            result += "Sum: " + getSum() + "\n";
+
+            result += "[ ";
+            for(int i = 0; i < getLength(); i++)
+            {
+                string number = getValueAt(i).ToString();
+                result += number + " ";
+            }
+            result += " ]";
+
+
+            return result;
+        }
+
+        public int getSum()
+        {
+            int sum = 0;
+            for (int i = 0; i < getLength(); i++)
+            {
+                sum += getValueAt(i);
+            }
+            return sum;
+        }
+
+        public int[] getValues()
+        {
+            return _values;
+        }
+
+        public int getValueAt(int index)
+        {
+            return _values[index];
+        }
+
+        public int getLength()
+        {
+            return _cursor - 1;
+        }
+
+        public int getBuffer()
+        {
+            return _buffer;
+        }
+
+        public void increaseSize()
+        {
+            _buffer *= 2;
+            Array.Resize<int>(ref _values, _buffer);
+        }
+    }
+}
---
+++ lab1-SumNumbers/lab1-SumNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab1_SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNumbers numbers = new MyNumbers();
+
+            Console.WriteLine("Enter list of positive numbers!!!");
+
+            string userInput = "";
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                userInput = Console.ReadLine();
+                finished = !int.TryParse(userInput, out int result);
+                numbers.add(result);
+            }
+
+            Console.WriteLine(numbers.getInfo());
+            
+
+        }
+    }
+}
