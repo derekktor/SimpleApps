2373db75ded502f89d80af7d79b2ef4a6c39321c
Derek <baldka1109bh@gmail.com>
2022-05-01 09:48:41 +0200

    Finished Max Weight Loss
    Reads text files as input
    Checks if only numbers are given as input
    Checks if number of days and number of weights match
    Finds the maximum difference between weights

--- lab1/MaxWeightLoss/MaxWeightLoss/Program.cs
+++ lab1/MaxWeightLoss/MaxWeightLoss/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace MaxWeightLoss
 {
@@ -13,10 +14,43 @@ namespace MaxWeightLoss
 
             string[] input = GetLines(Domain, FileCat, FileName);
 
+            int days = GetNumberOfDays(input[0]);
+            int[] weights = GetWeights(input[1]);
+
+            Debug.Assert(days == weights.Length, "Days and number of weights written don't match!");
+
+            WeightTracker wt = new WeightTracker(weights);
+
+            wt.GetInfo();
+
+            int maxDiff = wt.GetMaxDiff();
+
+            Console.WriteLine(maxDiff);
 
         }
 
-        public static bool DoLinesMatch()
+        public static int[] GetWeights(string line)
+        {
+            string[] textNums = line.Split(" ");
+            int[] weights = new int[textNums.Length];
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                bool isWeightValid = int.TryParse(textNums[i], out int w);
+                Debug.Assert(isWeightValid == true, "Weight has to be a number!");
+                weights[i] = w;
+            }
+            return weights;
+        }
+
+        public static int GetNumberOfDays(string FirstLine)
+        {
+            bool isDaysValid = int.TryParse(FirstLine, out int days);
+            Debug.Assert(isDaysValid == true, "First line has to be a number!");
+            return days;
+        }
+
+        //public static bool DoLinesMatch()
 
         public static string[] GetLines(string Domain, string FileCat, string FileName)
         {
--- lab1/MaxWeightLoss/MaxWeightLoss/WeightTracker.cs
+++ lab1/MaxWeightLoss/MaxWeightLoss/WeightTracker.cs
@@ -3,11 +3,43 @@ namespace MaxWeightLoss
 {
     public class WeightTracker
     {
+        int _days;
         int[] _weights;
 
         public WeightTracker(int[] weights)
         {
+            _days = weights.Length;
             _weights = weights;
+        }
+
+        public int GetMaxDiff()
+        {
+            int maxDiff = 0;
+            for (int i = 0; i < _days - 1; i++)
+            {
+                for (int j = i + 1; j < _days; j++)
+                {
+                    int sub = _weights[i] - _weights[j];
+                    if (sub > maxDiff)
+                    {
+                        maxDiff = sub;
+                    }
+                }
+            }
+
+            return maxDiff;
+        }
+
+        public void GetInfo()
+        {
+            string output = "";
+            output += "Days:\t" + _days.ToString() + "\n";
+            output += "Weights:\n\t";
+            foreach (int w in _weights)
+            {
+                output += w + " ";
+            }
+            Console.WriteLine(output);
         }
     }
 }
