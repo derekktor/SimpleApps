2d9bedefd6f54602eec7ed869fcfdf209aeb327e
Derek <baldka1109bh@gmail.com>
2022-05-01 10:40:06 +0200

    Finished Text Analysis app
    Takes in string as input
    Outputs the amount of occurence each character had in the string

---
+++ lab1/TextAnalysis/TextAnalysis/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<char> letters = new List<char>();
+
+            foreach (char c in input)
+            {
+                if (Char.IsWhiteSpace(c))
+                {
+                    continue;
+                }
+
+                if (!letters.Contains(c))
+                {
+                    letters.Add(c);
+                }
+            }
+
+            Dictionary<char, int> lettersDict = new Dictionary<char, int>();
+
+            foreach (char letter in letters)
+            {
+                int count = 0;
+                foreach (char c in input)
+                {
+                    if (letter == c)
+                    {
+                        count++;
+                    }
+                }
+                lettersDict.Add(letter, count);
+            }
+
+            foreach (var letter in lettersDict)
+            {
+                Console.WriteLine(letter);
+            }
+            
+        }
+    }
+}
